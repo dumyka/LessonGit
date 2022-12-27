270e2a64fddc36793096434dc7df10956f649f10
dumyka <dumyka1990@gmail.com>
2022-12-27 18:05:04 +0300

    changed SolutionTest

--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -11,10 +11,10 @@ public class SolutionTest {
       //открыть страницу гита
       open("https://github.com/");
       //выберите меню Solutions - enterprise
-      $(".HeaderMenu--logged-out").$$("ul li").findBy(text("Solutions"))
-          .hover().$("a").shouldHave(text("Enterprise")).click();
-      //$(".HeaderMenu--logged-out").$$("ul li").findBy(text("Solutions")).hover()
-      //        .$(".HeaderMenu-dropdown").$$("ul li").findBy(text("Enterprise")).click();
+      //$(".HeaderMenu--logged-out").$$("ul li").findBy(text("Solutions"))
+       //   .hover().$("a").shouldHave(text("Enterprise")).click();
+      $(".HeaderMenu--logged-out").$$("ul li").findBy(text("Solutions")).hover()
+              .$(".HeaderMenu-dropdown").$$("ul li").findBy(text("Enterprise")).click();
       //проверка, что заголовок - "Build like the best"
       $(".h1-mktg").shouldHave(text("Build like the best"));
     }
